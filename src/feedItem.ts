be2f1c46ba33543d105ece8c852a0811cf313f1e
Sam Lord <23644091+Slord6@users.noreply.github.com>
2024-03-10 11:51:02 +0000

    Parse RSS feeds to object

---
+++ src/feedItem.ts
@@ -0,0 +1,35 @@
+
+export class FeedItem {
+    private _title: string;
+    public get title(): string {
+        return this._title;
+    }
+    public set title(value: string) {
+        this._title = value;
+    }
+    private _url: URL;
+    public get url(): URL {
+        return this._url;
+    }
+    public set url(value: URL) {
+        this._url = value;
+    }
+    private _pubdate: string;
+    public get pubdate(): string {
+        return this._pubdate;
+    }
+    public set pubdate(value: string) {
+        this._pubdate = value;
+    }
+
+    constructor(title: string, url: URL, pubdate: string) {
+        this._title = title;
+        this._url = url;
+        this._pubdate = pubdate;
+    }
+
+    public toString(): string {
+        return `${this.title}(Published: ${this.pubdate})(${this.url.toString()})`;
+    }
+
+}
