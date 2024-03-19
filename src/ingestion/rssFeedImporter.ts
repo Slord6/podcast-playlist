54b1e9ae9f454f31b04356abe6826893913eafd3
Sam Lord <23644091+Slord6@users.noreply.github.com>
2024-03-19 20:22:32 +0000

    Add better logging and catches for feed parsing

--- src/ingestion/rssFeedImporter.ts
+++ src/ingestion/rssFeedImporter.ts
@@ -13,24 +13,26 @@ export class RSSFeedImporter {
     public async toFeed(): Promise<Feed | null> {
         const parser: Parser = new Parser();
         console.log(`(RSS) Parsing ${this.rssUrl}...`);
-        return BufferedRequests.fetch(this.rssUrl).then(r => r.text()).then(rssText => {
-            return parser.parseString(rssText).then(feed => {
-                const items: FeedItem[] = feed.items.map((item) => {
-                    const title = item.title ? item.title : "<Unknown title>";
-                    const urlLoc = item.enclosure?.url;
-                    const url = urlLoc ? new URL(urlLoc) : new URL("https://example.com"); // TODO better handling of missing URL
-                    return new FeedItem(title, url, item.pubDate ? item.pubDate : "");
+        return BufferedRequests.fetch(this.rssUrl).then(resp => resp.text(), (reason) => console.log(`(RSS) Fetch-parse of ${this.rssUrl} failed: ${reason}`))
+            .then(rssText => {
+                if(!rssText) return null;
+                return parser.parseString(rssText).then(feed => {
+                    const items: FeedItem[] = feed.items.map((item) => {
+                        const title = item.title ? item.title : "<Unknown title>";
+                        const urlLoc = item.enclosure?.url;
+                        const url = urlLoc ? new URL(urlLoc) : new URL("https://example.com"); // TODO better handling of missing URL
+                        return new FeedItem(title, url, item.pubDate ? item.pubDate : "");
+                    });
+
+                    const title = feed.title ? feed.title : "<Unknown title>";
+                    const imgUrl = feed.image?.url ? new URL(feed.image?.url) : new URL("https://example.com"); // TODO better handling of missing URL
+                    const feedObj = new Feed(title, this.rssUrl, new URL(feed.feedUrl as string), new URL(imgUrl), items);
+                    console.log(`(RSS) Feed "${title}" parsed`);
+                    return feedObj;
+                }).catch(reason => {
+                    console.error(`(RSS) Could not parse ${this.rssUrl}: ${reason}`);
+                    return null;
                 });
-    
-                const title = feed.title ? feed.title : "<Unknown title>";
-                const imgUrl = feed.image?.url ? new URL(feed.image?.url) : new URL("https://example.com"); // TODO better handling of missing URL
-                const feedObj = new Feed(title, this.rssUrl, new URL(feed.feedUrl as string), new URL(imgUrl), items);
-                console.log(`(RSS) Feed "${title}" parsed`);
-                return feedObj;
-            }).catch(reason => {
-                console.error(`(RSS) Could not parse ${this.rssUrl}: ${reason}`);
-                return null;
             });
-        });
     }
 }
