d4794f1343c89c68e38e0433e8734fbc5fa39fb2
Sam Lord <23644091+Slord6@users.noreply.github.com>
2024-04-10 21:48:40 +0100

    Identify file extensions in Downloader
    We extract mime type and convert to a file extension and fall back to '.bin' if all else fails.

--- src/downloader.ts
+++ src/downloader.ts
@@ -1,10 +1,12 @@
 import { FeedItem } from "./feedItem";
 import * as fs from "fs";
 import * as stream from "stream";
+import { MimeTypes } from "./mimeTypes";
 
 export class Downloader {
     private _source: URL;
     private _feedItem: FeedItem;
+
     constructor(feedItem: FeedItem, workingDir: string) {
         this._source = feedItem.url;
         this._feedItem = new FeedItem(feedItem.title, new URL(`file://${workingDir}/${Downloader.toSafeFileName(feedItem.title)}`), feedItem.pubdate, feedItem.author);
@@ -17,20 +19,39 @@ export class Downloader {
         return (unsafe.replace(/[ &\/\\#,+()$~%.'":*?<>{}\|]/g, ""));
     }
 
+    /**
+     * Get the extension for the podcast based on the 
+     * @returns Promise to extension string
+     */
+    private getExtension(): Promise<string> {
+        return fetch(this._source, {
+            method: "HEAD",
+            redirect: "follow"
+        }).then(response => {
+            return MimeTypes.getAudioExtension(response.headers.get("content-type"))
+        });
+    }
+
     public async download(): Promise<FeedItem> {
         return new Promise((resolve) => {
-            console.log(`(DOWNLOADER) Downloading ${this._feedItem.title} from ${this._source}...`);
-            const path = `${this._feedItem.url.host}/${this._feedItem.url.pathname}`;
-            if (fs.existsSync(path)) {
-                console.log(`(DOWNLOADER) File already exists, skipping (${this._feedItem.title})`);
-                return resolve(this._feedItem);
-            } else {
-                fetch(this._source).then(response => {
-                    return stream.Readable.fromWeb(response.body as any).pipe(fs.createWriteStream(path));
-                }).then((ws) => {
-                    ws.on("close", () => resolve(this._feedItem));
-                })
-            }
+            const extensionPromise = this.getExtension();
+            extensionPromise.then(extension => {
+                if(extension === "bin") {
+                    console.warn(`(DOWNLOADER) Could not determine extension from mime-type, will use 'bin'`);
+                }
+                const path = `${this._feedItem.url.host}/${this._feedItem.url.pathname}.${extension}`;
+                console.log(`(DOWNLOADER) Downloading ${this._feedItem.title} from ${this._source} to ${path}...`);
+                if (fs.existsSync(path)) {
+                    console.log(`(DOWNLOADER) File already exists, skipping (${this._feedItem.title})`);
+                    return resolve(this._feedItem);
+                } else {
+                    fetch(this._source).then(response => {
+                        return stream.Readable.fromWeb(response.body as any).pipe(fs.createWriteStream(path));
+                    }).then((ws) => {
+                        ws.on("close", () => resolve(this._feedItem));
+                    })
+                }
+            });
         });
     }
 }
---
+++ src/mimeTypes.ts
@@ -0,0 +1,25 @@
+export class MimeTypes {
+    private static _audioTypes: {[id: string]: string} = {
+        "aac": "aac",
+        "midi": "midi",
+        "mpeg": "mp3",
+        "ogg": "oga",
+        "opus": "opus",
+        "wav": "wav",
+        "webm": "weba",
+        "3gpp": "3gp",
+        "3gpp2": "3g2"
+    }
+
+    public static getAudioExtension(mime: string | null): string {
+        const defaultType = "bin";
+        if(mime === null) return defaultType;
+        
+        const mimeParts = mime.split("/");
+        const mimeType = mimeParts[0];
+        if(mimeType != "audio") {
+            return defaultType;
+        }
+        return this._audioTypes[mimeParts[1]] || defaultType;
+    }
+}
