2319a6b08be2cb65145a3706663698e041d24e0d
Sam Lord <23644091+Slord6@users.noreply.github.com>
2024-04-10 20:53:51 +0100

    Refactor to use PlaylistConfiguration

--- src/playlist/playheadFeed.ts
+++ src/playlist/playheadFeed.ts
@@ -12,21 +12,22 @@ export class PlayheadFeed {
         return this._playhead >= this._unplayed.length;
     }
     public get next(): FeedItem | null {
-        if(this.listened) return null;
+        if (this.listened) return null;
         return this._unplayed[this._playhead];
     }
-    public get nextUnsafe(): FeedItem {;
+    public get nextUnsafe(): FeedItem {
         return this._unplayed[this._playhead];
     }
+    private canIncludeFilter: (feedItem: FeedItem) => boolean;
 
 
-    constructor(feed: Feed, history: History) {
+    constructor(feed: Feed, history: History, canIncludeFilter: (feedItem: FeedItem) => boolean) {
         this._feed = feed;
         this._history = history;
-        this._unplayed = this._feed.items.filter(i => !this._history.listenedToByFeedItem(i)).sort((a,b) => {
+        this._unplayed = this._feed.items.filter(i => !this._history.listenedToByFeedItem(i)).sort((a, b) => {
             const dateA = a.published;
             const dateB = b.published;
-            if(dateA === null || dateB === null) {
+            if (dateA === null || dateB === null) {
                 console.warn(`(PlayheadFeed) Cannot determine episode order of [${a}] and [${b}]`);
                 return 0;
             } else {
@@ -34,10 +35,19 @@ export class PlayheadFeed {
             }
         })
         this._playhead = 0;
+        this.canIncludeFilter = canIncludeFilter;
     }
 
     public skip() {
-        this._playhead ++;
+        do {
+            this._playhead++;
+        } while (!this.listened && this.canIncludeFilter(this.nextUnsafe));
+    }
+
+    public randomUnlistened(ignore: FeedItem[] = []): FeedItem {
+        const availableItems = this._unplayed.filter(unplayed => !ignore.includes(unplayed));
+        const index = Math.round(Math.random() * (availableItems.length - 1));
+        return availableItems[index];
     }
 
 }
