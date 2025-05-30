29f597d3de9e08468a3162b37f95704708098b4c
Sam <github@samlord.co.uk>
2025-05-30 14:34:50 +0100

    Fix handling of feed image and web URLs

--- src/ingestion/rssFeedImporter.ts
+++ src/ingestion/rssFeedImporter.ts
@@ -18,19 +18,35 @@ export class RSSFeedImporter {
         return BufferedRequests.fetch(this.rssUrl).then(resp => resp.text(), (reason) => console.error(`(RSS) Fetch-parse of ${this.rssUrl} failed: ${reason}`))
             .then(rssText => {
                 if (!rssText) return null;
+                RSSFeedImporter._logger(`Got feed document`, "VeryVerbose");
                 return parser.parseString(rssText).then(feed => {
+                    RSSFeedImporter._logger(`Feed inital parse successfuk`, "VeryVerbose");
                     const feedTitle = feed.title ? feed.title : "<Unknown Author>";
                     const items: FeedItem[] = feed.items.map((item) => {
-                        const title = item.title ? item.title : "<Unknown title>";
-                        const urlLoc = item.enclosure?.url;
-                        const url = urlLoc ? new URL(urlLoc) : new URL("https://example.com"); // TODO better handling of missing URL
-                        return new FeedItem(title, url, item.pubDate ? item.pubDate : "", feedTitle, item.itunes?.episodeType || "full", item.enclosure?.type || null);
+                        try {
+                            const title = item.title ? item.title : "<Unknown title>";
+                            const urlLoc = item.enclosure?.url;
+                            const url = urlLoc ? new URL(urlLoc) : new URL("https://example.com"); // TODO better handling of missing URL
+                            return new FeedItem(title, url, item.pubDate ? item.pubDate : "", feedTitle, item.itunes?.episodeType || "full", item.enclosure?.type || null);
+                        } catch (err) {
+                            throw new Error(`Failed parsing feed item ${item} (caused by: ${err})`);
+                        }
                     });
 
-                    const imgUrl = feed.image?.url ? new URL(feed.image?.url) : new URL("https://example.com"); // TODO better handling of missing URL
-                    const feedObj = new Feed(feedTitle, this.rssUrl, new URL(feed.feedUrl as string), new URL(imgUrl), items);
-                    RSSFeedImporter._logger(`Feed "${feedTitle}" parsed`, "Verbose");
-                    return feedObj;
+                    try {
+                        const webPageSrc = feed.feedUrl || feed.link;
+                        const imgSrc = feed.image?.url || feed.itunes?.image;
+                        RSSFeedImporter._logger(`Feed info URLs:\n\t${webPageSrc}\n\t${imgSrc}`, "VeryVerbose")
+
+                        const webUrl = webPageSrc ? new URL(webPageSrc) : new URL("https://example.com"); // TODO better handling of missing URL
+                        const imgUrl = imgSrc ? new URL(imgSrc) : new URL("https://example.com"); // TODO better handling of missing URL
+
+                        const feedObj = new Feed(feedTitle, this.rssUrl, webUrl, imgUrl, items);
+                        RSSFeedImporter._logger(`Feed "${feedTitle}" parsed`, "Verbose");
+                        return feedObj;
+                    } catch (err) {
+                        throw new Error(`Could not construct Feed object from parsed data (caused by: ${err})`);
+                    }
                 }).catch(reason => {
                     console.error(`(RSS) Could not parse ${this.rssUrl}: ${reason}`);
                     return null;
