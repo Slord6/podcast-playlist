d89f8076913c57756c75d9a933b8e7101b5bd2cf
Sam <github@samlord.co.uk>
2026-03-08 21:10:43 +0000

    Version bump to 1.2.0

--- src/index.ts
+++ src/index.ts
@@ -211,7 +211,7 @@ const argv = yargs(helpers.hideBin(process.argv))
     .alias("veryverbose", "vv")
     .describe("veryverbose", "High verbosity of output")
     .demandCommand(1, 1)
-    .version("1.1.1")
+    .version("1.2.0")
     .parse() as any;
 
 if (argv.veryverbose) {
